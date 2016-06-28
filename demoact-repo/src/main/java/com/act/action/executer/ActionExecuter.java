c0f7b10df467b5eca3c2e53d92162dd07e7529b9
Ankita Pandey <ankita.pandey@eisenvault.com>
2016-06-28 15:14:30 +0530

    OCR Link in folder

---
+++ demoact-repo/src/main/java/com/act/action/executer/ActionExecuter.java
@@ -0,0 +1,26 @@
+package com.act.action.executer;
+
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ActionDefinition;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+public interface ActionExecuter {
+	
+	/**
+	    * Get the action definition for the action
+	    * 
+	    * @return  the action definition
+	    */
+	   public ActionDefinition getActionDefinition();
+		
+	   /**
+	    * Execute the action executer
+	    * 
+	    * @param action				the action
+	    * @param actionedUponNodeRef	the actioned upon node reference
+	    */
+	   public void execute(
+	     	Action action,
+	        NodeRef actionedUponNodeRef);
+
+}
